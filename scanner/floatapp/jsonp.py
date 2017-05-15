289794e51423d39a606ad0e47355cb826195ede2
sum12 <sumitjami@gmail.com>
2017-05-15 08:58:50 +0200

    bufdo retab

--- scanner/floatapp/jsonp.py
+++ scanner/floatapp/jsonp.py
@@ -6,13 +6,13 @@ import re
 jsonp_validator = re.compile("^[a-zA-Z0-9_\-.]{1,128}$")
 
 def jsonp(f):
-	"""Wraps JSONified output for JSONP"""
-	@wraps(f)
-	def decorated_function(*args, **kwargs):
-		callback = request.args.get('callback', False)
-		if callback and jsonp_validator.match(callback):
-			content = str(callback) + '(' + str(f(*args,**kwargs).data) + ')'
-			return current_app.response_class(content, mimetype='application/javascript')
-		else:
-			return f(*args, **kwargs)
-	return decorated_function
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback and jsonp_validator.match(callback):
+            content = str(callback) + '(' + str(f(*args,**kwargs).data) + ')'
+            return current_app.response_class(content, mimetype='application/javascript')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
--- scanner/main.py
+++ scanner/main.py
@@ -6,18 +6,18 @@ import sys
 import os
 
 def main():
-	reload(sys)
-	sys.setdefaultencoding("UTF-8")
+    reload(sys)
+    sys.setdefaultencoding("UTF-8")
 
-	if len(sys.argv) != 3:
-		print "usage: %s ALBUM_PATH CACHE_PATH" % sys.argv[0]
-		return
-	try:
-		os.umask(022)
-		TreeWalker(sys.argv[1], sys.argv[2])
-	except KeyboardInterrupt:
-		message("keyboard", "CTRL+C pressed, quitting.")
-		sys.exit(-97)
-	
+    if len(sys.argv) != 3:
+        print "usage: %s ALBUM_PATH CACHE_PATH" % sys.argv[0]
+        return
+    try:
+        os.umask(022)
+        TreeWalker(sys.argv[1], sys.argv[2])
+    except KeyboardInterrupt:
+        message("keyboard", "CTRL+C pressed, quitting.")
+        sys.exit(-97)
+    
 if __name__ == "__main__":
-	main()
+    main()
