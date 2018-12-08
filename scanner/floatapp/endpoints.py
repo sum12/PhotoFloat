01a312d6b5905d0ffc34f1ad17cb8ff95fbc6e74
sum12 <sumitjami@gmail.com>
2018-12-08 22:11:27 +0100

    no need to wait for scanner, just upload

--- scanner/floatapp/endpoints.py
+++ scanner/floatapp/endpoints.py
@@ -141,9 +141,6 @@ def photos():
 @app.route("/upload", methods=['POST'])
 @jsonp
 def upload():
-    global worker
-    if worker is not None and worker.is_alive():
-        abort(make_response(jsonify(code='msg', msg='scan is running, please wait'), 409))
     if request.form.get('album_path', '') == '':
         abort(make_response(jsonify(code='msg', msg='album path is missing'), 400))
     if 'pic' not in request.files:
